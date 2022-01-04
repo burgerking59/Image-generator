30935984efaa8a57072b8d70db322e2181869c38
Oliver Burnett-Kiernan <78424718+burgerking59@users.noreply.github.com>
2022-01-04 20:12:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function load() {
         }
         document.getElementById("area").innerHTML += '<br>'
     }
-    baseImg = ["22", "52", "25", "35", "45", "55"]
+    eyes = ["22", "52"]
+    mouth = ["25", "35", "45", "55"]
     hair = ["00", "10", "20", "30", "40", "50", "60", "70"]
 }
 
@@ -20,17 +21,15 @@ function randomize() {
             var randomColor = Math.floor(Math.random() * 16777215).toString(16);
             var randomHair = Math.floor(Math.random() * 16777215).toString(16);
             var randomEyes = Math.floor(Math.random() * 10)
+            var randomMouthCol = Math.floor(Math.random() * 16777215).toString(16);
+            var randomMouth = Math.floor(Math.random() * 10)
             for (let x = 0; x < 8; x++) {
                 for (let y = 0; y < 8; y++) {
                     colour = false
-                    for (let z = 0; z < baseImg.length; z++) {
-                        if (document.getElementById(String(x) + String(y)) == document.getElementById(baseImg[z])) {
+                    for (let z = 0; z < eyes.length; z++) {
+                        if (document.getElementById(String(x) + String(y)) == document.getElementById(eyes[z])) {
                             if (randomEyes == 1) {
-                                if (String(x) + String(y) == "22" || String(x) + String(y) == "52") {
-                                    document.getElementById(String(x) + String(y)).style.backgroundColor = "white";
-                                } else {
-                                    document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
-                                }
+                                document.getElementById(String(x) + String(y)).style.backgroundColor = "white";
                             } else {
                                 document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
                             }
@@ -38,8 +37,19 @@ function randomize() {
                             break
                         }
                     }
-                    for (let w = 0; w < hair.length; w++) {
-                        if (document.getElementById(String(x) + String(y)) == document.getElementById(hair[w])) {
+                    for (let z = 0; z < mouth.length; z++) {
+                        if (document.getElementById(String(x) + String(y)) == document.getElementById(mouth[z])) {
+                            if (randomMouth == 1) {
+                                document.getElementById(String(x) + String(y)).style.backgroundColor = "#" + randomMouthCol;
+                            } else {
+                                document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
+                            }
+                            colour = true
+                            break
+                        }
+                    }
+                    for (let z = 0; z < hair.length; z++) {
+                        if (document.getElementById(String(x) + String(y)) == document.getElementById(hair[z])) {
                             document.getElementById(String(x) + String(y)).style.backgroundColor = "#" + randomHair;
                             colour = true
                             break
