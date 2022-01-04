2f908f5a751f87fa49e71cc8de8afc75ac2fdee1
Oliver Burnett-Kiernan <78424718+burgerking59@users.noreply.github.com>
2022-01-04 15:52:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,36 +8,36 @@ function load() {
     }
     baseImg = ["22", "52", "25", "35", "45", "55"]
     hair = ["00", "10", "20", "30", "40", "50", "60", "70"]
-    count = 0
 }
 
 function randomize() {
-    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    var randomHair = Math.floor(Math.random() * 16777215).toString(16);
-    for (let x = 0; x < 8; x++) {
-        for (let y = 0; y < 8; y++) {
-            colour = false
-            for (let z = 0; z < baseImg.length; z++) {
-                if (document.getElementById(String(x) + String(y)) == document.getElementById(baseImg[z])) {
-                    document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
-                    colour = true
-                    break
+    for (v = 0; v < 100; v++) {
+        var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        var randomHair = Math.floor(Math.random() * 16777215).toString(16);
+        for (let x = 0; x < 8; x++) {
+            for (let y = 0; y < 8; y++) {
+                colour = false
+                for (let z = 0; z < baseImg.length; z++) {
+                    if (document.getElementById(String(x) + String(y)) == document.getElementById(baseImg[z])) {
+                        document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
+                        colour = true
+                        break
+                    }
                 }
-            }
-            for (let w = 0; w < hair.length; w++) {
-                if (document.getElementById(String(x) + String(y)) == document.getElementById(hair[w])) {
-                    document.getElementById(String(x) + String(y)).style.backgroundColor = "#" + randomHair;
-                    colour = true
-                    break
+                for (let w = 0; w < hair.length; w++) {
+                    if (document.getElementById(String(x) + String(y)) == document.getElementById(hair[w])) {
+                        document.getElementById(String(x) + String(y)).style.backgroundColor = "#" + randomHair;
+                        colour = true
+                        break
+                    }
                 }
-            }
-            if (colour == false) {
-                document.getElementById(String(x) + String(y)).style.backgroundColor = "#" + randomColor;
+                if (colour == false) {
+                    document.getElementById(String(x) + String(y)).style.backgroundColor = "#" + randomColor;
+                }
             }
         }
+        finalImg = document.getElementById("area").cloneNode(true)
+        finalImg.id = "area" + String(v)
+        document.body.appendChild(finalImg)
     }
-    count++
-    finalImg = document.getElementById("area").cloneNode(true)
-    finalImg.id = "area" + String(count)
-    document.body.appendChild(finalImg)
 }
