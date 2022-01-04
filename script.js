53c0aab1d1ede7a05a4d32ca0b401d8e86083a3d
Oliver Burnett-Kiernan <78424718+burgerking59@users.noreply.github.com>
2022-01-04 20:05:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -19,12 +19,21 @@ function randomize() {
         for (j = 0; j < 10; j++) {
             var randomColor = Math.floor(Math.random() * 16777215).toString(16);
             var randomHair = Math.floor(Math.random() * 16777215).toString(16);
+            var randomEyes = Math.floor(Math.random() * 10)
             for (let x = 0; x < 8; x++) {
                 for (let y = 0; y < 8; y++) {
                     colour = false
                     for (let z = 0; z < baseImg.length; z++) {
                         if (document.getElementById(String(x) + String(y)) == document.getElementById(baseImg[z])) {
-                            document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
+                            if (randomEyes == 1) {
+                                if (String(x) + String(y) == "22" || String(x) + String(y) == "52") {
+                                    document.getElementById(String(x) + String(y)).style.backgroundColor = "white";
+                                } else {
+                                    document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
+                                }
+                            } else {
+                                document.getElementById(String(x) + String(y)).style.backgroundColor = "black";
+                            }
                             colour = true
                             break
                         }
